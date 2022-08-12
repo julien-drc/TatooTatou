763e7d7d34f4285d64fae8a4451a1edcc9d285e2
julien dumont <lapz95@hotmail.fr>
2022-08-12 19:11:22 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent, useState } from 'react';
+
+const App: FunctionComponent = () => {
+  const [name, setName] = useState('React')
+
+  return (
+    <h1>Hello, {name} !</h1>
+  )
+}
+
+export default App;
