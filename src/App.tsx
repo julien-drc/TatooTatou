262dff08b378b2e68965c703dc6e7348e215397a
julien dumont <lapz95@hotmail.fr>
2022-08-17 20:38:46 +0200

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import React, { FunctionComponent} from 'react';
 import PokemonList from './pages/pokemon-list';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import PokemonsDetail from './pages/pokemon-detail';
+import PageNotFound from './pages/page-not-found';
 
 const App: FunctionComponent = () => {
 
   return (
-    <PokemonList/>
+    <Router>
+      <div>
+        <nav>
+          <div className='nav-wrapper teal'>
+            <Link to="/" className='brand-logo center'>Pokédex</Link>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={PokemonList}></Route>
+          <Route exact path="/pokemons" component={PokemonList}></Route>
+          <Route path="/pokemons/:id" component={PokemonsDetail}></Route>
+          <Route component={PageNotFound}/>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
