6f40621e1bbe9f68efda76df182d2c0930ec66b0
julien dumont <lapz95@hotmail.fr>
2022-08-17 18:07:32 +0200

    add type=2

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,6 +1,8 @@
 import React, { FunctionComponent, useState } from 'react';
 import Pokemon from '../models/pokemon';
-import './pokemon-card.css'
+import './pokemon-card.css';
+import formatDate from '../helpers/format-date'
+import formatType from '../helpers/format-type'
 
 type Props = {
   pokemon: Pokemon
@@ -27,7 +29,10 @@ const PokemonCard: FunctionComponent<Props> = ({ pokemon, borderColor = '#009688
         <div className="card-stacked">
           <div className="card-content">
             <p>{pokemon.name}</p>
-            <p><small>{pokemon.created.toString()}</small></p>
+            <p><small>{formatDate(pokemon.created)}</small></p>
+            {pokemon.types.map(type => (
+              <span key={type} className={formatType(type)}>{type}</span>
+            ))}
           </div>
         </div>
       </div>
